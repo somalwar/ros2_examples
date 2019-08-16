a2018b8906d695f08e2a9a482a84ae13bbc8df0e
somalwar <mihirsomalwar@gmail.com>
2019-08-16 16:37:13 -0700

    final updates

--- gnss_state/include/gnss_state/state_node.hpp
+++ gnss_state/include/gnss_state/state_node.hpp
@@ -2,7 +2,8 @@
 #define GNSS_STATE_STATE_NODE_HPP_
 
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"//#include "gnss_state/msg/state.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "gnss_state/msg/state.hpp"
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
 #include "sensor_msgs/msg/imu.hpp"
 #include "visibility.h"
@@ -15,12 +16,12 @@ class GnssStateNode : public rclcpp::Node
 
     private:
     void publisher_callback();
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Publisher<gnss_state::msg::State>::SharedPtr publisher_;
     rclcpp::Subscription<sensor_msgs::msg::NavSatFix>::SharedPtr sat_subscription_;
     rclcpp::Subscription<sensor_msgs::msg::Imu>::SharedPtr imu_subscription_;
     rclcpp::TimerBase::SharedPtr timer_;
     sensor_msgs::msg::NavSatFix nav_sat_msg_;
-    sensor_msgs::msg::Imu imu_msg;
+    sensor_msgs::msg::Imu imu_msg_;
 };
 
 #endif
--- gnss_state/src/state_node.cpp
+++ gnss_state/src/state_node.cpp
@@ -1,7 +1,8 @@
 #include <chrono>
 
 #include "../include/gnss_state/state_node.hpp"
-#include "std_msgs/msg/string.hpp" //#include "gnss_state/msg/state.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "gnss_state/msg/state.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/nav_sat_fix.hpp"
 #include "sensor_msgs/msg/imu.hpp"
@@ -14,19 +15,31 @@ GnssStateNode::GnssStateNode(rclcpp::NodeOptions options)
     sat_subscription_ = create_subscription<sensor_msgs::msg::NavSatFix>(
         "navsatfix_topic", 10, [this](sensor_msgs::msg::NavSatFix::UniquePtr msg) {
         RCLCPP_INFO(this->get_logger(), "I heard on navsatfix_topic");
+        nav_sat_msg_ = *msg;
         });
     imu_subscription_ = create_subscription<sensor_msgs::msg::Imu>(
         "imu_topic", 10, [this](sensor_msgs::msg::Imu::UniquePtr msg) {
         RCLCPP_INFO(this->get_logger(), "I heard on imu_topic");
+        imu_msg_ = *msg;
         });
-    publisher_ = create_publisher<std_msgs::msg::String>("state_topic", 10);
+    publisher_ = create_publisher<gnss_state::msg::State>("state_topic", 10);
     timer_ = this->create_wall_timer(500ms, std::bind(&GnssStateNode::publisher_callback, this));
 }
 
 void GnssStateNode::publisher_callback()
 {
-    auto message = std_msgs::msg::String();
-    message.data = "Publishing on to state_topic";
-    RCLCPP_INFO(this->get_logger(), "%s", message.data.c_str());
+    auto message = gnss_state::msg::State();
+    message.x = nav_sat_msg_.longitude;
+    message.y = nav_sat_msg_.latitude;
+    message.z = nav_sat_msg_.altitude;
+    message.roll = imu_msg_.orientation.x;
+    message.pitch = imu_msg_.orientation.y;
+    message.yaw = imu_msg_.orientation.z;
+    //message.linear_velocity = 
+    message.angular_velocity.z = imu_msg_.angular_velocity.z;
+    //message.linear_acceleration = 
+    message.angular_acceleration.z = imu_msg_.linear_acceleration.z;
+
+    RCLCPP_INFO(this->get_logger(), "Publishing message onto state_topic");
     publisher_->publish(message);
 }
